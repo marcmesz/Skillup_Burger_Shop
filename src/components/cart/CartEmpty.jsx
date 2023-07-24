35042bd0d65905db5ef5616f81a366228b5bf0b7
Mészöly Márton <meszoly.marton@gmail.com>
2023-07-24 17:34:11 +0200

    Update in responsive design

--- src/components/cart/CartEmpty.jsx
+++ src/components/cart/CartEmpty.jsx
@@ -4,9 +4,9 @@ import { Link } from "react-router-dom";
 
 const CartEmpty = () => {
     return (
-        <article className="cart-empty d-flex flex-column align-items-center">
+        <article className="cart-empty d-flex flex-column align-items-center px-0">
             <FiShoppingCart />
-            <h3 className="text-center m-5">Your cart is empty.</h3>
+            <h3 className="text-center my-5">Your cart is empty.</h3>
             <p><Link to="/" className="btn btn-lg btn-outline-primary">Go back to explore the menu</Link></p>
         </article>
     )
