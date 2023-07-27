fafef01445dfa8148959d8159c666ac8ef8061ea
Mészöly Márton <meszoly.marton@gmail.com>
2023-07-27 16:53:12 +0200

    CSS hotfix for Safari

--- src/components/home/Menu.jsx
+++ src/components/home/Menu.jsx
@@ -33,30 +33,6 @@ const Menu = () => {
                             />
                         )
                     })}
-                    {/* <MenuCard
-                        itemNum={1}
-                        burgerSrc={burger1}
-                        price={200}
-                        title="Cheese Burger"
-                        handler={addToCartHandler}
-                        delay={0.1}
-                    />
-                    <MenuCard
-                        itemNum={2}
-                        burgerSrc={burger2}
-                        price={500}
-                        title="Veg Cheese Burger"
-                        delay={0.5}
-                        handler={addToCartHandler}
-                    />
-                    <MenuCard
-                        itemNum={3}
-                        burgerSrc={burger3}
-                        price={1800}
-                        title="Cheese Burger with French Fries"
-                        delay={0.8}
-                        handler={addToCartHandler}
-                    /> */}
                 </div>
             </section>
         </div>
