25e9d13eaebe50e56850b72d33b24f99512b8dff
Mészöly Márton <meszoly.marton@gmail.com>
2023-08-04 13:13:33 +0200

    Update Footer.jsx
    Fancy non-working font-family only working on safari changed to uniformed style for consistency
    
    Responsive footer layout changed.

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -6,23 +6,32 @@ import { AiFillInstagram, AiFillYoutube, AiFillGithub } from "react-icons/ai";
 const Footer = () => {
     return (
         <footer>
-            <main className="container-xxl">
-                <div>
-                    <h2>Burger Shop</h2>
-                    <p>We are trying to give you the best taste possible.</p>
-                    <br />
-                    <em>We give attention to genuine feedback.</em>
-                    <strong>All right received @burgershop</strong>
+            <main className="container-xxl p-0">
+                <div className="footer-content d-flex justify-content-between">
+                    <div className="footer-content__left d-flex flex-column">
+                        <h2 className="my-3 mt-0">Burger Shop</h2>
+                        <p className="my-3">We are trying to give you the best taste possible.</p>
+                        <p className="my-3"><em>We give attention to genuine feedback.</em></p>
+                    </div>
+                    <aside className="d-flex flex-column align-items-center">
+                        <h4>Follow Us</h4>
+                        <p>
+                            <a href="https://www.youtube.com/marcmesz" target="_blank">
+                                <AiFillYoutube />
+                            </a>
+                            <a href="https://www.instagram.com/marcmesz" target="_blank">
+                                <AiFillInstagram />
+                            </a>
+                            <a href="https://www.github.com/marcmesz" target="_blank">
+                                <AiFillGithub />
+                            </a>
+                        </p>
+                    </aside>
                 </div>
-                <aside>
-                    <h4>Follow Us</h4>
-                    <a href="https://youtube.com/xyz">
-                        <AiFillYoutube />
-                    </a>
-                    <a href="https://instagram.com/xyz">
-                        <AiFillInstagram />
-                    </a>
-                </aside>
+                <div className="copy-text d-flex justify-content-between mt-5">
+                    <strong>&copy; 2023 All rights reserved @burgershop</strong>
+                    <p className="mx-2">Made by Mészöly Márton</p>
+                </div>
             </main>
         </footer>
     )
